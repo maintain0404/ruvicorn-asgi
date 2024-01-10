91ced6534ea8da07f66244d7c89d469aeee2b86c
Taein Min <maintain0404@gmail.com>
2024-01-10 17:23:17 +0900

    🧑‍💻 Add import hook before serving h11

--- ruvicorn_core/scripts/serve_h11.py
+++ ruvicorn_core/scripts/serve_h11.py
@@ -1,3 +1,13 @@
+from maturin import import_hook
+
+# install the import hook with default settings
+import_hook.install()
+# or you can specify bindings
+import_hook.install(bindings="pyo3")
+# and build in release mode instead of the default debug mode
+import_hook.install(release=True)
+
+
 from ruvicorn_core import Http11Protocol
 import asyncio as aio
 
